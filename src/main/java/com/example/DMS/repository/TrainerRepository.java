e1a41c3c3455ad79663d55284eb8242425d8107e
PRANEETH SHANIGARAM <156373777+spraneeth28@users.noreply.github.com>
2024-11-24 18:44:08 -0600

    Added DogsManagementSystem files

---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+/**
+ * @author Praneeth Shanigaram
+ */
+package com.example.DMS.repository;
+import org.springframework.data.repository.CrudRepository;
+import com.example.DMS.Models.Trainer;
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
